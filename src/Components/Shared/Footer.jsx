2249e4170c8493ee02eae39046f0249c2874297b
Sycada <pantho0>
2023-12-30 16:58:26 +0600

    navtop & footer color profile updated

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -2,7 +2,7 @@ import CurrentYear from "../Utils/CurrentYear";
 
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 bg-blue-600 text-white">
+    <footer className="footer footer-center p-4 bg-gradient-to-r from-sky-500 to-indigo-500 text-white">
       <aside>
         <CurrentYear/>
       </aside>
